296951bbe84cf6a3040750806b723ab15952f663
Juan Eugenio Abadie <abadiejuan@hotmail.com>
2016-08-24 00:48:33 -0300

    se agrega script de chequeo de errores en logs

---
+++ util/errors.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# script de impresion de errores por consola
+
+print_help() {
+    cat << EOF
+Modo de uso: errors [OPCION]...
+    Si no se especifica ninguna opcion, imprime los errores del dia
+    OPCIONES:
+        -y  imprime los errores del dia anterior
+        -h  esta ayuda
+EOF
+}
+
+while getopts "yh" option; do
+    case $option in
+        y) DAY=$(date +%Y-%m-%d -d "yesterday");;
+        h) print_help && exit;;
+    esac
+done
+
+grep --text --no-filename -E "production.ERROR" /var/www/wspuertos/storage/logs/laravel-${DAY:-$(date +%Y-%m-%d)}*.log 2> /dev/null
