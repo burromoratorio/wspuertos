22a55d605b3cc4f10e3e7bf9ffe3be293538f474
Juan Eugenio Abadie <abadiejuan@hotmail.com>
2016-09-15 04:36:31 -0300

    Adapta despliegues a Lumes

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -5,44 +5,22 @@
 @endsetup
 
 @task('init', ['on' => 'server'])
+
     cd /var/www/
     git clone git@gitserver.siacseguridad.com:/var/git/sistemas/{{ "$project" }}.git
     cd {{ "$project" }}
     chmod -R ugo+w storage bootstrap/cache
     ln -s .env.produccion .env
     composer install --no-dev
-    npm install --production
-    node_modules/gulp/bin/gulp.js --production
-    php artisan optimize --force
-    # php artisan route:cache # no soporta closures
-    php artisan config:cache
+
 @endtask
 
 @task('deploy', ['on' => 'server'])
 
     cd /var/www/{{ "$project" }}
     git fetch origin master
-
-    echo "--- Se instalan/actualizan dependencias (npm). Esto puede tardar... ---"
-    git checkout FETCH_HEAD -- npm-shrinkwrap.json package.json
-    npm install --production
-
-    php artisan down
-
     echo "--- Se instalan/actualizan dependencias (composer) ---"
-    git checkout FETCH_HEAD -- composer.json composer.lock
-    composer install --no-dev
-
-    echo "--- Se compilan assets, se actualiza aplicación, se crean caches ---"
     git merge origin master
-    node_modules/gulp/bin/gulp.js --production
-    php artisan optimize --force
-    # php artisan route:cache # no soporta closures
-    php artisan config:cache
-
-    # por si algun template blade no existe mas
-    php artisan view:clear
-
-    php artisan up
+    composer install --no-dev
 
 @endtask
