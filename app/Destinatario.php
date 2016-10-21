2a373da8572548d0af41d22d753b0eb9d46095cc
Diego Meschini <dmeschini@siacseguridadcom>
2016-10-21 17:21:43 -0300

    Modifico modelo

--- app/Destinatario.php
+++ app/Destinatario.php
@@ -7,5 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Destinatario extends Model
 {
     protected $table = 'destinatarios';
+
+    protected $guarded = ['id'];
+    protected $fillable = array('cliente_id', 'mail');
+
+    protected $dates = ['created_at','updated_at'];
     protected $dateFormat = 'Y-m-d H:i:s';
+
+    public function clientes() {
+        return $this->belongsTo('App\Cliente', 'cliente_id');
+    }
+
 }
