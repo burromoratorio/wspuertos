479918a7aa07a0fe05fe73dc510a5f74958a24e6
Diego Meschini <dmeschini@siacseguridadcom>
2016-10-21 17:22:07 -0300

    Agrego rutas para el modelo destinatarios

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -8,3 +8,8 @@ $app->get('eventos', 'EventoController@index');
 $app->post('eventos', 'EventoController@store');
 
 $app->patch('avisos/{id}', 'AvisoController@update');
+
+$app->get('destinatarios', 'DestinatarioController@index');
+$app->post('destinatarios', 'DestinatarioController@store');
+$app->patch('destinatarios/{id}', 'DestinatarioController@update');
+$app->delete('destinatarios/{id}', 'DestinatarioController@destroy');
