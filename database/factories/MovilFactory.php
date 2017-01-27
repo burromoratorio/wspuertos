22194452eb0dfc006e7550b372c125752d41a0b9
Juan Eugenio Abadie <abadiejuan@hotmail.com>
2017-01-27 02:44:12 -0300

    Agrega más factories para pruebas

--- database/factories/MovilFactory.php
+++ database/factories/MovilFactory.php
@@ -18,15 +18,14 @@ $factory->define(App\Cliente::class, function (Faker\Generator $faker) {
         'razon_social' => $faker->name,
         'localidad_id' => function () {
             return factory(App\Localidad::class)->create()->localidad_id;
-        }
+        },
+        // terminar si se necesita
     ];
 });
 
 $factory->define(App\Movil::class, function (Faker\Generator $faker) {
     return [
-        'cliente_id' => function () {
-            return factory(App\Cliente::class)->create()->cliente_id;
-        }
+        'cliente_id' => 1,
     ];
 });
 
@@ -39,9 +38,7 @@ $factory->define(App\Waypoint::class, function (Faker\Generator $faker) {
 $factory->define(App\AvisoCliente::class, function (Faker\Generator $faker) {
     return [
         'aviso_tipo_id' => 1, // entrada waypoint
-        'cliente_id' => function () {
-            return factory(App\Cliente::class)->create()->cliente_id;
-        },
+        'cliente_id' => 1,
     ];
 });
 
@@ -59,9 +56,7 @@ $factory->define(App\AvisoConfiguracion::class, function (Faker\Generator $faker
 
 $factory->define(App\Destinatario::class, function (Faker\Generator $faker) {
     return [
-        'cliente_id' => function () {
-            return factory(App\Cliente::class)->create()->id;
-        },
+        'cliente_id' => 1,
         'mail' => $faker->email,
     ];
 });
@@ -116,5 +111,34 @@ $factory->state(App\ReenvioMovil::class, 'udp', function ($faker) {
                 'protocolo' => 'UDP',
             ])->id;
         },
+    ];
+});
+
+$factory->define(App\ReenvioPosicion::class, function (Faker\Generator $faker) {
+    return [
+        'movil_id' => 1,
+        'cadena' => 'PC110816051106LXG512-32.94920-60.67649000000101+00+00+00|',
+    ];
+});
+
+$factory->define(App\ReenvioPosicionHost::class, function (Faker\Generator $faker) {
+    return [
+        'reenvio_posicion_id' => function () {
+            return factory(App\ReenvioPosicion::class)->create()->id;
+        },
+        'reenvio_host_id' => function () {
+            return factory(App\ReenvioHost::class)->create()->id;
+        },
+        'estado_envio_id' => 1, // ESTADO_PENDIENTE
+    ];
+});
+
+$factory->define(App\Aviso::class, function (Faker\Generator $faker) {
+    return [
+        'aviso_cliente_id' => function () {
+            return factory(App\AvisoCliente::class)->create()->id;
+        },
+        'aviso' => 'SIAC - JRB877 ingresa al waypoint: DETENCION_PROHIBIDA_JUNIN;Hora de ingreso: 26/09/2016 19:01:45',
+        'estado_envio_id' => 1, // ESTADO_PENDIENTE
     ];
 });
