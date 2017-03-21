4f8a44549591187e2fc6e14f77ba1513502a107d
Alan Moratorio -Via Api-test <root@gitServer.siacseguridad>
2017-03-21 10:13:01 -0300

    envio a don pedro por protocolo caessat 2017 con dominios de 7 digitos, a los demas por caessat anterior

--- app/Http/Controllers/ReenvioController.php
+++ app/Http/Controllers/ReenvioController.php
@@ -6,7 +6,9 @@ use Illuminate\Http\Request;
 
 use App\ReenvioPosicion;
 use App\ReenvioPosicionHost;
+use DB;
 use Carbon\Carbon;
+Use Log;
 use Illuminate\Support\Facades\Redis;
 class ReenvioController extends Controller
 {
@@ -17,40 +19,51 @@ class ReenvioController extends Controller
     public function index(Request $request) {
         return ReenvioPosicion::take(30)->get();
     }
-
-    public function store(Request $request) {
+/*test con desicion de formacion de cadena*/
+public function store(Request $request) {
         //{"movil_id":"11849","hora":"1462346654","patente":"LXG508","latitud":"32.949092","longitud":"60.676610","velocidad":"0.000000","sentido":"269.120000","posGpsValida":"1","evento":"1","temperatura1":"22","temperatura2":"23","temperatura3":"24"}
-        $reenvioPosicion = ReenvioPosicion::create([
+
+        $hostMovil = DB::table('reenvios_moviles')->where('movil_id',$request->input('movil_id'))
+                                                  ->where('activo','1')
+                                                  ->join('reenvios_hosts','reenvios_moviles.reenvio_host_id','reenvios_hosts.id')->get();
+        foreach($hostMovil as $host){
+          Log::error($host->destino);
+
+          if($host->destino=="200.55.7.172"){
+            $cadena=$this->mkCaessatString17($request->all());
+          }else{
+            $cadena=$this->mkCaessatString($request->all());
+          }
+
+          $reenvioPosicion = ReenvioPosicion::create([
             'movil_id' => $request->input('movil_id'),
-            'cadena' => $this->mkCaessatString($request->all()),
-        ]);
-        $reenvioPosicion
-            ->reenvios_moviles
-            ->filter(function($value) { return $value->activo == '1' ;})
-            ->each(function ($reenvio_movil) use ($reenvioPosicion) {
-                $reenvioPosicionHost = ReenvioPosicionHost::create([
+            'cadena'=>$cadena
+          ]);
+
+          $reenvioPosicionHost = ReenvioPosicionHost::create([
                     'reenvio_posicion_id' => $reenvioPosicion->id,
-                    'reenvio_host_id' => $reenvio_movil->reenvio_host_id,
+                    'reenvio_host_id' => $host->reenvio_host_id,
                     'estado_envio_id' => static::ESTADO_PENDIENTE,
-                ]);
-                $reenvioHost = $reenvioPosicionHost->reenvio_host;
-                $this->publishToRedis(
+          ]);
+          $this->publishToRedis(
                     $reenvioPosicionHost->id,
-                    $reenvioHost->destino,
-                    $reenvioHost->puerto,
+                    $host->destino,
+                    $host->puerto,
                     $reenvioPosicion->cadena,
-                    $reenvioHost->protocolo
+                    $host->protocolo
                 );
-            });
+ 
+
+        }
         return "OK\n";
-    }
+}
 
     private function checkLength($name, $field, $length) {
         if (strlen($field) != $length)
             throw new \Exception("Longitud incorrecta del campo: ".$name." - ".$field);
         return $field;
     }
-
+    
     private function mkCaessatString(array $fields) {
         //PC251210104844HRA450-34.70557-058.49464018360101+00+00+00
         $cadena =
@@ -67,6 +80,24 @@ class ReenvioController extends Controller
             $this->checkLength("temperatura2", sprintf("%+03d", $fields['temperatura2'] > 99 ? 99 : $fields['temperatura2']), 3).
             $this->checkLength("temperatura3", sprintf("%+03d", $fields['temperatura3'] > 99 ? 99 : $fields['temperatura3']), 3).
             "|";
+ 	Log::error($cadena);           
+        return $cadena;
+    }
+    private function mkCaessatString17(array $fields) {
+        //ABC123,010114210000,-12.34567,+012.34567,80,180,005,100850000,-2,4,-1
+        $cadena =
+            $fields['patente'].",".
+            $this->checkLength("fecha", Carbon::createFromTimestamp($fields['hora'])->format('dmyHis'), 12).",".
+            $this->checkLength("latitud", sprintf("%+09.5f", $fields['latitud']), 9).",".
+            $this->checkLength("longitud", sprintf("%+010.5f", $fields['longitud']), 10).",".
+            $this->checkLength("velocidad", sprintf("%03d", $fields['velocidad']), 3).",".
+            $this->checkLength("sentido", sprintf("%03d", $fields['sentido']), 3).",".
+            $this->checkLength("evento", sprintf("%02d", $fields['evento']), 2).",".
+            "0,".
+            $this->checkLength("temperatura1", sprintf("%+03d", $fields['temperatura1'] > 99 ? 99 : $fields['temperatura1']), 3).",".
+            $this->checkLength("temperatura2", sprintf("%+03d", $fields['temperatura2'] > 99 ? 99 : $fields['temperatura2']), 3).",".
+            $this->checkLength("temperatura3", sprintf("%+03d", $fields['temperatura3'] > 99 ? 99 : $fields['temperatura3']), 3)."|";
+ 	Log::error($cadena);           
         return $cadena;
     }
 
@@ -90,6 +121,7 @@ class ReenvioController extends Controller
 
     protected function publishToRedis($id, $host, $port, $msg,$proto) {
         if($proto=='TCP'){
+            Log::error("publicando"); 
 	    Redis::publish('caessat', json_encode(compact('id', 'host', 'port', 'msg','proto')));
 	}else{
 	    Redis::publish('caessat-udp',json_encode(compact('id','host','port','msg','proto')) );
